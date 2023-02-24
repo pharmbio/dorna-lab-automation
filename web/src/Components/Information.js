813670e6de371b993d10a89363ac3c8f2bedbae0
rikard <rikardnystrom95@gmail.com>
2023-02-24 12:40:52 +0100

    new checklist points

--- web/src/Components/Information.js
+++ web/src/Components/Information.js
@@ -7,11 +7,15 @@ export default function Information(props) {
       <div className="container">
         <ol>
           <li>Open flowbot door _completely_!</li>
-          <li>Move microscope to its loading position.</li>
-          <li>Place a test plate in microscope.</li>
-          <li>Turn on Dorna control box.</li>
-          <li>Mount plexiglass shield around the workbench.</li>
-          <li>Verify progress with TA.</li>
+          <li>If off, turn on Dorna control box</li>
+          <li>Mount plexiglass shield around the workbench</li>
+          <li>Move microscope to its loading position</li>
+          <li>If not in safe position, move dorna to safe using above button</li>
+          <li>Open Dorna Lab in other tab. ONLY if in safe position => Turn off motors</li>
+          <li>Physically move Dorna into safe position, zeroing all joints</li>
+          <li>Click "Set Joint", then "Set All" in Dorna Lab</li>
+          <li>Turn on motors again</li>
+          <li>Verify progress with TA...</li>
         </ol>
         <ul>
 	  <li>Move to a plate position using the selector above and Move button below.</li>
