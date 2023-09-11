bbd721c6e5e89beba41c68ff9b5c8a8d0d45492d
slaide <patrick_hennig@fastmail.com>
2023-09-11 17:06:31 +0200

    refactored gripper presets

--- server/gripperFunctions.py
+++ server/gripperFunctions.py
@@ -1,59 +1,57 @@
 import json
-
-port = str(0)
-pwm, duty, freq = "pwm"+port, "duty"+port, "freq"+port
+import typing as tp
+from dataclasses import dataclass
 
 calibrationfile = "../parameters.json"
 
-# This function can be run between each pickup/place
-# to have the changes work in real-time
-def updateServoSettings(move):
-    default = {
-            "prepare": 9.5,
-            "grip": 8.5,
-            "release": 10,
-            "wide": 12
-    }
-    try:
-        file = open(calibrationfile, "r")
-        data = json.load(file)
-        duty = data.get(move)
-        return duty 
-    except FileNotFoundError:
-        print("No calibration file found, using default values")
-    except OSError as e:
-        # Catch other errors, such as permissions etc.
-        print(f"Unable to open {calibrationfile}: {e}")
+DEFAULT_GRIPPER_DUTY = {
+    "prepare": 9.5,
+    "grip": 8.5,
+    "release": 10,
+    "wide": 12
+}
 
-    return default[move]
+@dataclass
+class GripperPreset:
+    name: str
+    move_duration: float = 0.5
+    """ estimated move duration in s. required because the motor itself does not provide any status indicator as to its state """
+        
+    def read_properties_from_disk(self)->float:
+        """ read gripper properties (just 'duty', i.e. grip width, for now) from disk """
+
+        duty:float = DEFAULT_GRIPPER_DUTY[self.name]
+
+        try:
+            with open(calibrationfile, "r") as file:
+                data = json.load(file)
+                duty = data.get(self.name)
+        except FileNotFoundError:
+            print("No calibration file found, using default values")
+        except OSError as e:
+            # Catch other errors, such as permissions etc.
+            print(f"Unable to open {calibrationfile}: {e}")
+
+        return duty
+
+    def __call__(self,robot:"Dorna")->tp.Any:
+        gripper_pwm_duty=self.read_properties_from_disk()
+
+        # gripper on port 0 -> play command argument suffix 0 (pwm0, freq0, duty0)
+        # pwm0:1 -> make sure gripper motor is on
+        # freq0:50 -> from motor spec
+        # duty0: <somvalue> -> set gripper width to target value (note: value does not correspond to a real-world distance directly)
+        kwargs = {"cmd": "pwm", "pwm0": 1, "freq0": 50, "duty0": gripper_pwm_duty}
+        status = robot.play(**kwargs)
+        robot.sleep(self.move_duration)
+        return status
+
 
 # Prepare for microplate pickup
-def prepare(r):
-    duty = updateServoSettings("prepare")
-    kwargs = {"cmd": "pwm", "pwm0": 1, "freq0": 50, "duty0": duty}
-    status = r.play(**kwargs)
-    return status
-
+prepare=GripperPreset("prepare")
 # Grip microplate
-def grip(r):
-    duty = updateServoSettings("grip")
-    kwargs = {"cmd": "pwm", "pwm0": 1, "freq0": 50, "duty0": duty}
-    status = r.play(**kwargs)
-    r.sleep(0.5)
-    return status
-
+grip=GripperPreset("grip")
 # Release microplate
-def release(r):
-    duty = updateServoSettings("release")
-    kwargs = {"cmd": "pwm", "pwm0": 1, "freq0": 50, "duty0": duty}
-    status = r.play(**kwargs)
-    r.sleep(0.5)
-    return status
-
+release=GripperPreset("release")
 # Fully open gripper
-def wide(r):
-    duty = updateServoSettings("wide")
-    kwargs = {"cmd": "pwm", "pwm0": 1, "freq0": 50, "duty0": duty}
-    status = r.play(**kwargs)
-    r.sleep(3)
-    return status
+wide=GripperPreset("wide",move_duration=3.0)
