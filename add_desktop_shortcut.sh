ce8deeedd0f368b777c6882edb881279b9d8f767
slaide <patrick_hennig@fastmail.com>
2023-10-05 18:16:22 +0200

    add script to install desktop shortcut

---
+++ add_desktop_shortcut.sh
@@ -0,0 +1,9 @@
+echo '[Desktop Entry]
+Type=Application
+Terminal=false
+Name=Dorna
+Icon=utilities-terminal
+Exec=/home/pharmbio/Downloads/dorna-lab-automation/run.sh
+Categories=Application;
+' > ~/Desktop/dorna.desktop
+chmod 755 ~/Desktop/dorna.desktop run.sh
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+cd ~/Downloads/dorna-lab-automation
+
 gnome-terminal -- bash -c "cd server ; source env/bin/activate ; python3 server.py"
 gnome-terminal -- bash -c "cd web ; npm start"
