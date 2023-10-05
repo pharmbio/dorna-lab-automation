e19fea8af6ed02696293af7ff9367dbfa5c6f205
slaide <patrick_hennig@fastmail.com>
2023-10-05 16:43:32 +0200

    updated preflight checklist

--- web/src/Components/Components/Preflight.js
+++ web/src/Components/Components/Preflight.js
@@ -5,12 +5,18 @@ export default function Preflight() {
         <h1>Preflight checklist</h1>
         <div className="container">
           <ol>
-            <li>Open flowbot door _completely_!</li>
-            <li>If off, turn on Dorna control box</li>
-            <li>Move microscope to its loading position</li>
-            <li>Physically move Dorna into safe position, zeroing all joints</li>
+            <li>
+              These are performed by a TA
+              <ol>
+                <li>Turn on Dorna control box</li>
+                <li>Connect microscope and put it into its loading position</li>
+                <li>Physically move Dorna into safe position, then calibrate all joints</li>
+              </ol>
+            </li>
+            <li>Open flowbot door <b>completely!</b></li>
+            <li>Put the well plates you will be using later into the flowbot</li>
             <li>Mount plexiglass shield around the workbench</li>
-            <li>Verify progress with teaching assisstant</li>
+            <li>Do not reach over the plexiglass shield while it is installed</li>
           </ol>
         </div>
       </div>
